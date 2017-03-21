6c74068794fab2bfbb9cb140132bfb49b6429708
Evan Schultz <evan@rangle.io>
2017-03-21 16:20:28 -0400

    meaningful commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <ActionBar title="My App!?"></ActionBar>
+    <ActionBar title="Better Title"></ActionBar>
     <!-- Your UI components go here -->
   `
 })
