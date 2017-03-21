1cc375b82d6479371313d7f04a92a212639170fc
Evan Schultz <evan@rangle.io>
2017-03-21 14:11:06 -0400

    nvm

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "******** Install NVM *************"
-brew install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 echo "******** Install the node I want *************"
 nvm install v6.10.10
 # Install NativeScript CLI
