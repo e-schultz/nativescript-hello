7c55e546dfabad5262786f98e8b71eea6d8916fb
Evan Schultz <evan@rangle.io>
2017-03-21 14:07:57 -0400

    meh try it again

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+echo "******** Install NVM *************"
+brew install nvm
+echo "******** Install the node I want *************"
+nvm install v6.10.10
+# Install NativeScript CLI
+echo "******** Install NativeScript CLI *************"
+npm install -g nativescript
+
+# NativeScript Ready
+echo "******** NativeScript CLI Ready *************"
+tns --version
+
+# Prepare NativeScript Project
+echo "******** Prepare NativeScript iOS Project *************"
+tns prepare ios
